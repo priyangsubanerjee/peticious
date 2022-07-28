89f22eb6500e61938a6cb1bc5a48bfae3e4b2b02
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-07-28 20:45:22 +0530

    bg-gray

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Container({ children }) {
-  return <div className="bg-slate-100 pb-32">{children}</div>;
+  return <div className="bg-gray-100 pb-32">{children}</div>;
 }
 
 export default Container;
