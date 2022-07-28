99f6163c5cb51c18f035733511dd30134cb9a177
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-07-28 20:41:43 +0530

    bg-slate

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Container({ children }) {
-  return <div className="bg-slate-50 pb-32">{children}</div>;
+  return <div className="bg-slate-100 pb-32">{children}</div>;
 }
 
 export default Container;
